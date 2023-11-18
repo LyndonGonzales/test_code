d13b5ed3f1102de80cc41b3c00d5241dd9bfd14d
LyndonGonzales <lyndongo@hotmail.com>
2023-11-18 10:27:27 -0600

    Add files via upload

---
+++ setup.bat
@@ -0,0 +1,7 @@
+ECHO OFF
+REM USE x86_64  not the i686 stuff!!!
+ECHO ON
+
+SET PATH=C:\Users\lyndong\Desktop\CPP\mingw32\bin;C:\Users\lyndong\Desktop\CPP\SDL2-2.26.2\x86_64-w64-mingw32\bin;%PATH%;
+SET INCLUDE=C:\Users\lyndong\Desktop\CPP\mingw32\include;
+SET LIB=C:\Users\lyndong\Desktop\CPP\mingw32\lib;
